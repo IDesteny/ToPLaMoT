2d825c48b94f134be463e008680cd20e2d2b0871
ilya <t1m0shenko@yandex.ru>
2022-12-05 12:52:21 +0300

    refact

--- ToPLaMoT/Compiler.cs
+++ ToPLaMoT/Compiler.cs
@@ -11,7 +11,7 @@ namespace ToPLaMoT
 		static readonly string ExecutableName = string.Concat(OutputFilename, ".exe");
 		static readonly string SourceFilename = string.Concat(OutputFilename, ".cs");
 
-		static readonly string CompilerPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe";
+		static public string CompilerPath = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\Roslyn\csc.exe";
 
 		static public async Task<(bool executionStatus, string compilerReportMsg)> CompileAsync(string sourceCSCode)
 		{
@@ -30,13 +30,17 @@ namespace ToPLaMoT
 
 			if (!process.ExitCode.Equals(0))
 			{
-				var errorMessage = await process.StandardOutput.ReadToEndAsync();
-				var trimInfo = errorMessage.Remove(0, errorMessage.LastIndexOf(":") + 2);
-
-				return (true, trimInfo);
+				return (true, await GetErrorMessage(process));
 			}
 
 			return (false, string.Empty);
+		}
+
+		static async Task<string> GetErrorMessage(Process process)
+		{
+			var errorMessage = await process.StandardOutput.ReadToEndAsync();
+
+			return errorMessage[(errorMessage.LastIndexOf(':') + 2)..];
 		}
 
 		static public Task RunAsync()
